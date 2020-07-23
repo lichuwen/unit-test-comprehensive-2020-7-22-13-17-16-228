7d82798989f0972424c7ec8b782fdf4cb8deff49
lichuwen <545759585@qq.com>
2020-07-23 19:13:52 +0800

    [karen] feature:add should_return_4A0B_when_guess_1234_given_input_1234 test

---
+++ src/test/java/tdd/GuessNumberGameTest.java
@@ -0,0 +1,22 @@
+package tdd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GuessNumberGameTest {
+    @Test
+    void should_return_4A0B_when_guess_1234_given_input_1234(){
+        //given
+        String guessNumber = "1234";
+        GuessNumberGame guessNumberGame = new GuessNumberGame();
+
+        //when
+        String actual = guessNumberGame.guess();
+
+        //then
+        assertEquals("4A0B",actual);
+
+    }
+
+
+}
