273c793dd9baee95adb460114d6ca443fb14fa56
lichuwen <545759585@qq.com>
2020-07-25 09:52:36 +0800

    [karen] modify:GuessNumberGameTest use mock

--- src/test/java/tdd/GuessNumberGameTest.java
+++ src/test/java/tdd/GuessNumberGameTest.java
@@ -2,15 +2,20 @@ package tdd;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 public class GuessNumberGameTest {
     @Test
     void should_return_4A0B_when_guess_given_answer_1234_and_input_guess_1234(){
         //given
-        String answer = "1234";
+        RadomAnswerGenerator mockradomAnswerGenerator = Mockito.mock(RadomAnswerGenerator.class);
+        given(mockradomAnswerGenerator.generateRadomAnswer()).willReturn("1234");
+
         String guessNumber = "1234";
-        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        GuessNumberGame guessNumberGame = new GuessNumberGame(mockradomAnswerGenerator.generateRadomAnswer());
 
         //when
         String actual = guessNumberGame.guess(guessNumber);
@@ -23,9 +28,11 @@ public class GuessNumberGameTest {
     @Test
     void should_return_0A0B_when_guess_5678_given_input_1234(){
         //given
-        String answer = "1234";
+        RadomAnswerGenerator mockradomAnswerGenerator = Mockito.mock(RadomAnswerGenerator.class);
+        given(mockradomAnswerGenerator.generateRadomAnswer()).willReturn("1234");
+
         String guessNumber = "5678";
-        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        GuessNumberGame guessNumberGame = new GuessNumberGame(mockradomAnswerGenerator.generateRadomAnswer());
 
         //when
         String actual = guessNumberGame.guess(guessNumber);
@@ -38,9 +45,11 @@ public class GuessNumberGameTest {
     @Test
     void should_return_0A2B_when_guess_2156_given_input_1234(){
         //given
-        String answer = "1234";
+        RadomAnswerGenerator mockradomAnswerGenerator = Mockito.mock(RadomAnswerGenerator.class);
+        given(mockradomAnswerGenerator.generateRadomAnswer()).willReturn("1234");
+
         String guessNumber = "2156";
-        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        GuessNumberGame guessNumberGame = new GuessNumberGame(mockradomAnswerGenerator.generateRadomAnswer());
 
         //when
         String actual = guessNumberGame.guess(guessNumber);
@@ -53,9 +62,11 @@ public class GuessNumberGameTest {
     @Test
     void should_return_0A4B_when_guess_3412_given_input_1234(){
         //given
-        String answer = "1234";
+        RadomAnswerGenerator mockradomAnswerGenerator = Mockito.mock(RadomAnswerGenerator.class);
+        given(mockradomAnswerGenerator.generateRadomAnswer()).willReturn("1234");
+
         String guessNumber = "3412";
-        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        GuessNumberGame guessNumberGame = new GuessNumberGame(mockradomAnswerGenerator.generateRadomAnswer());
 
         //when
         String actual = guessNumberGame.guess(guessNumber);
@@ -68,9 +79,11 @@ public class GuessNumberGameTest {
     @Test
     void should_return_2A2B_when_guess_1243_given_input_1234(){
         //given
-        String answer = "1234";
+        RadomAnswerGenerator mockradomAnswerGenerator = Mockito.mock(RadomAnswerGenerator.class);
+        given(mockradomAnswerGenerator.generateRadomAnswer()).willReturn("1234");
+
         String guessNumber = "1243";
-        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        GuessNumberGame guessNumberGame = new GuessNumberGame(mockradomAnswerGenerator.generateRadomAnswer());
 
         //when
         String actual = guessNumberGame.guess(guessNumber);
@@ -83,9 +96,11 @@ public class GuessNumberGameTest {
     @Test
     void should_return_1A1B_when_guess_1563_given_input_1234(){
         //given
-        String answer = "1234";
+        RadomAnswerGenerator mockradomAnswerGenerator = Mockito.mock(RadomAnswerGenerator.class);
+        given(mockradomAnswerGenerator.generateRadomAnswer()).willReturn("1234");
+
         String guessNumber = "1563";
-        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        GuessNumberGame guessNumberGame = new GuessNumberGame(mockradomAnswerGenerator.generateRadomAnswer());
 
         //when
         String actual = guessNumberGame.guess(guessNumber);
